7076f86556f53c4e040642ad3ebce076cafe2ff7
amitnos123 <sami.nosatzki@gmail.com>
2020-02-26 17:12:07 +0200

    Added focus on click for windows

--- GUI/Window/Window.gd
+++ GUI/Window/Window.gd
@@ -1,15 +1,22 @@
 extends Control
 
-#export(String) var labelText setget set_label_text, get_label_text
+signal move_to_top
+
+var drag_position = null
 
 func _ready():
 	pass
 
-#func set_label_text(value):
-#	labelNodePath.text = value
-#
-#func get_label_text():
-#	return labelNodePath.text
-
 func _on_CloseButton_pressed():
 	self.set_visible(false)
+
+func drag_window(event): #_on_gui_input
+	if event is InputEventMouseButton:
+		if event.pressed:
+			drag_position = get_global_mouse_position() - rect_global_position
+			emit_signal('move_to_top', self)
+		else:
+			drag_position = null
+
+	if event is InputEventMouse and drag_position:
+		rect_global_position = get_global_mouse_position() - drag_position
